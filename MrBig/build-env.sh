3528742546e4c7a27fddbe54e7439433da5be128
STSM01 <STSM01@ce5a70da-38e7-1b4f-8ac2-21067b8d50b1>
2014-02-24 07:13:30 +0000

    Updated paths to use a newer toolchain.
    git-svn-id: http://qbsvn01:8080/svn/qbranch-mrbig/trunk@44 ce5a70da-38e7-1b4f-8ac2-21067b8d50b1

--- MrBig/build-env.sh
+++ MrBig/build-env.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-W32_TOOLS_DIR=/cygdrive/m/toolchain/mingw-w32-1.0-bin_i686-mingw_20100702/bin
-W64_TOOLS_DIR=/cygdrive/m/toolchain/mingw-w64-1.0-bin_i686-mingw_20100702/bin
+W32_TOOLS_DIR=/cygdrive/d/mingw/x32-4.8.1-posix-sjlj-rev5/mingw32/bin
+W64_TOOLS_DIR=/cygdrive/d/mingw/x64-4.8.1-posix-seh-rev5/mingw64/bin
 export PATH=${W32_TOOLS_DIR}:${W64_TOOLS_DIR}:/bin
 echo "Type make now."
 export PS1='build-env:${PWD##*/}> '
